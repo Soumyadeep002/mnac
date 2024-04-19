26edba8b2f34e4b559d10b2c51b7cdcce93e5e6b
soumyadeep002 <soumyadeepganguly1922@gmail.com>
2024-04-19 17:04:48 +0530

    navbar updated

--- js/script.js
+++ js/script.js
@@ -7,6 +7,39 @@
  // end of preloader 
 
 
+ let mobnav = document.querySelector("#mobnav");
+let menu_btn = document.querySelector("#menu_btn");
+let line1 = document.querySelector("#line1");
+let line2 = document.querySelector("#line2");
+let line3 = document.querySelector("#line3");
+let topbar = document.querySelector("#topbar");
+
+
+
+menu_btn.addEventListener("click", function () {
+    mobnav.classList.toggle("left-[0%]");
+    mobnav.classList.toggle("left-[100%]");
+
+    line1.classList.toggle("rotate-down")
+    line2.classList.toggle('hide');
+    line3.classList.toggle("rotate-up")
+     
+    
+  })
+
+  let mobnavitm = document.querySelectorAll(".mobnav-itm")
+  for (let i = 0; i < mobnavitm.length; i++) {
+    mobnavitm[i].addEventListener("click", function (){
+        mobnav.classList.toggle("left-[0%]");
+        mobnav.classList.toggle("left-[100%]");
+
+        line1.classList.toggle("rotate-down")
+        line2.classList.toggle('hide');
+        line3.classList.toggle("rotate-up")
+    })
+  }
+
+
 
  const navbar = document.querySelector("#nav")
         const menulist = document.querySelector("#menulist")
