857d894a0d832d21e4119169ba851d1c67a1bb34
soumyadeep002 <soumyadeepganguly1922@gmail.com>
2024-09-24 11:56:48 +0530

    text and navbar updated

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
  // end of preloader 
 
 
- let mobnav = document.querySelector("#mobnav");
+let mobnav = document.querySelector("#mobnav");
 let menu_btn = document.querySelector("#menu_btn");
 let line1 = document.querySelector("#line1");
 let line2 = document.querySelector("#line2");
@@ -15,44 +15,16 @@ let line3 = document.querySelector("#line3");
 let topbar = document.querySelector("#topbar");
 
 
-
-menu_btn.addEventListener("click", function () {
-    mobnav.classList.toggle("left-[0%]");
-    mobnav.classList.toggle("left-[100%]");
-
-    line1.classList.toggle("rotate-down")
-    line2.classList.toggle('hide');
-    line3.classList.toggle("rotate-up")
-     
-    
-  })
-
-  let mobnavitm = document.querySelectorAll(".mobnav-itm")
-  for (let i = 0; i < mobnavitm.length; i++) {
-    mobnavitm[i].addEventListener("click", function (){
-        mobnav.classList.toggle("left-[0%]");
-        mobnav.classList.toggle("left-[100%]");
-
-        line1.classList.toggle("rotate-down")
-        line2.classList.toggle('hide');
-        line3.classList.toggle("rotate-up")
-    })
-  }
-
-
-
- const navbar = document.querySelector("#nav")
-        const menulist = document.querySelector("#menulist")
-        var menuline = document.querySelectorAll(".menu-line")
-        const blacklogo = document.querySelector("#blacklogo")
-        const whitelogo = document.querySelector("#whitelogo")
-        // const backtotop = document.getElementById("backtotop")
+const navbar = document.querySelector("#nav")
+const menulist = document.querySelector("#menulist")
+var menuline = document.querySelectorAll(".menu-line")
+const blacklogo = document.querySelector("#blacklogo")
+const whitelogo = document.querySelector("#whitelogo")
 
 
         window.addEventListener('scroll', function(){
             let value = window.scrollY;
             if(value > 100){
-            //   navbar.classList.remove("bg-blue-800");
             navbar.classList.add("bg-white");
             menulist.classList.remove("md:text-white");
             menulist.classList.add("md:text-black");
@@ -60,13 +32,10 @@ menu_btn.addEventListener("click", function () {
             blacklogo.classList.remove("hidden")
             whitelogo.classList.add("hidden")
             whitelogo.classList.remove("block")
-            // backtotop.classList.remove("hidden")
-
                 for (let i = 0; i < menuline.length; i++) {
                     menuline[i].classList.remove("bg-white")
                     menuline[i].classList.add("bg-black")
                 }
-
             }
             else{
             navbar.classList.remove("bg-white");
@@ -84,3 +53,36 @@ menu_btn.addEventListener("click", function () {
             }
 
         })
+
+        menu_btn.addEventListener("click", function () {
+            mobnav.classList.toggle("left-[0%]");
+            mobnav.classList.toggle("left-[100%]");
+            if(window.scrollY < 100){
+                navbar.classList.toggle("bg-white");
+                menulist.classList.toggle("md:text-white");
+                menulist.classList.toggle("md:text-black");
+                blacklogo.classList.toggle("block")
+                blacklogo.classList.toggle("hidden")
+                whitelogo.classList.toggle("hidden")
+                whitelogo.classList.toggle("block")
+                    for (let i = 0; i < menuline.length; i++) {
+                        menuline[i].classList.toggle("bg-white")
+                        menuline[i].classList.toggle("bg-black")
+                    }
+            }
+            line1.classList.toggle("rotate-down")
+            line2.classList.toggle('hide');
+            line3.classList.toggle("rotate-up")
+          })
+        
+          let mobnavitm = document.querySelectorAll(".mobnav-itm")
+          for (let i = 0; i < mobnavitm.length; i++) {
+            mobnavitm[i].addEventListener("click", function (){
+                mobnav.classList.toggle("left-[0%]");
+                mobnav.classList.toggle("left-[100%]");
+        
+                line1.classList.toggle("rotate-down")
+                line2.classList.toggle('hide');
+                line3.classList.toggle("rotate-up")
+            })
+          }
